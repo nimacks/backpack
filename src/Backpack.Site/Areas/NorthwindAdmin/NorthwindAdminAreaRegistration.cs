6bc4cfbcfdbd96eff7b123ba54ca285b8449be4d
Samuel Mensah <samuelnmensah@live.com>
2015-12-22 23:29:30 -0800

    added  data model

---
+++ src/Backpack.Site/Areas/NorthwindAdmin/NorthwindAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Backpack.Site.Areas.NorthwindAdmin
+{
+    public class NorthwindAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "NorthwindAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "NorthwindAdmin_default",
+                "NorthwindAdmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
