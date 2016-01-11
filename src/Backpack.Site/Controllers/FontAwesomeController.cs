6e6dde6b3c8014d408df701315a78232cdb350e5
Samuel Mensah <samuel.nimako-mensah@popagency.com>
2016-01-11 13:19:01 -0800

    added font awesome controller

---
+++ src/Backpack.Site/Controllers/FontAwesomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Backpack.Site.Controllers
+{
+    public class FontAwesomeController : Controller
+    {
+        // GET: FontAwesome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
