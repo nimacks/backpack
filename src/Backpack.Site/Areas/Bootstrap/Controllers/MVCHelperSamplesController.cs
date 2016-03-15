1b77ffef9559ff562846535290913a19f6c53059
Samuel Mensah <samuel.nimako-mensah@popagency.com>
2016-03-15 09:17:55 -0700

    added mvchelper

---
+++ src/Backpack.Site/Areas/Bootstrap/Controllers/MVCHelperSamplesController.cs
@@ -0,0 +1,55 @@
+﻿using System.Web.Mvc;
+using Backpack.Site.Areas.Bootstrap.Models;
+
+namespace Backpack.Site.Areas.Bootstrap.Controllers
+{
+    public class MVCHelperSamplesController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Helper01()
+        {
+            return View();
+        }
+
+        public ActionResult Helper02()
+        {
+            UserData model = new UserData();
+
+            return View(model);
+        }
+
+        public ActionResult Helper03()
+        {
+            UserData model = new UserData();
+
+            return View(model);
+        }
+
+        public ActionResult Helper04()
+        {
+            UserData model = new UserData();
+
+            return View(model);
+        }
+
+        public ActionResult Helper05()
+        {
+            UserData model = new UserData();
+
+            return View(model);
+        }
+
+        public ActionResult Helper06()
+        {
+            UserData model = new UserData();
+
+            return View(model);
+        }
+    }
+}
