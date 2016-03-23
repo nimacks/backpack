58c7d7b9c1bd6ec4b770145895661fdbc92e6b62
Samuel Mensah <samuelnmensah@live.com>
2016-03-23 15:34:31 -0700

    added new blog controller to test tinymce

--- src/Backpack.Site/Controllers/HomeController.cs
+++ src/Backpack.Site/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Backpack.Site.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Hello Your application description page.";
 
             return View();
         }
---
+++ src/Backpack.Site/Models/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Backpack.Site.Models
+{
+    public class BlogPost
+    {
+        public string Title { get; set; }
+        public DateTime PostedOn { get; set; }
+        public string Tags { get; set; }
+
+        [UIHint("tinymce_jquery_full"), AllowHtml]
+        public string Content { get; set; }
+    }
+}
